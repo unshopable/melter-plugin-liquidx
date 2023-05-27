5412119d089d69642b319bb0df6078b87b8eaeab
davidthorand <david.thorand@thorborn.com>
2023-05-27 20:35:50 +0200

    Only render content when necessary

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ export class LiquidXPlugin extends Plugin {
   apply(compiler: Compiler): void {
     compiler.hooks.emitter.tap('LiquidXPlugin', (emitter: Emitter) => {
       emitter.hooks.beforeAssetAction.tap('LiquidXPlugin', (asset: Asset) => {
-        asset.content = Buffer.from(render(asset.content.toString()));
+        if (asset.action !== 'remove') {
+          asset.content = Buffer.from(render(asset.content.toString()));
+        }
       });
     });
   }
